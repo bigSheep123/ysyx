79298cb468b0075f924636f7632827d607403678
bigSheep123 <1554869970@qq.com>
2025-03-12 09:59:14 +0800

    save

---
+++ Verilog/test.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "example.v"
+
+int main()
+{
+    while (1)
+    {
+        int a = rand() & 1;
+        int b = rand() & 1;
+        top->a = a;
+        top->b = b;
+        top->eval();
+        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+        assert(top->f == (a ^ b));
+    }
+}
