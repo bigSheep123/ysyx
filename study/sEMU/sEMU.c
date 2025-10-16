ea61f7f0900b994ca76fe98694306965b1bf3500
bigSheep123 <1554869970@qq.com>
2025-10-17 00:37:32 +0800

    sEMU

--- study/sEMU/sEMU.c
+++ study/sEMU/sEMU.c
@@ -8,16 +8,26 @@ uint8_t PC = 0;
 uint8_t R[4];
 uint8_t M[16];
 
+// 0:li rθ，10   # 这里是十进制的10
+// l:li rl，θ
+// 2:li r2，θ
+// 3: li r3,1
+// 4: add rl,rl,r3
+// 5: add r2,r2,r1
+// 6:bner0 r1，4
+// 7:out  
+// 8:bnere r3,7
+
 // Regs:  R[0-3]  00 01 10 11
-//  00 rd rs1 rs2    add 指令 
-//  10 rd imm        R[rd]=imm
-//  11 addr   rs2    
+//  00 rd rs1 rs2    add 指令        add
+//  10 rd imm        R[rd]=imm       li
+//  11 addr   rs2    bner0           bner0
+//  01                               out  
 
 void init_Mem() {
-    M[0] = 0b10000001;
-    M[1] = 0b10010010;
-    M[2] = 0b00110001;
-    M[3] = 0b11000000; 
+    M[0] = 0b10001010,M[1] = 0b10010000,M[2] = 0b10100000,M[3] = 0b10110001,
+    M[4] = 0b00010111,M[5] = 0b00101001,M[6] = 0b11010100,M[7] = 0b01000000,
+    M[8] = 0b11111000;
 }
 
 void inst_cycle() {
@@ -30,13 +40,18 @@ void inst_cycle() {
         uint8_t reg = (inst >> 4)&(0x03);
         R[reg] = (inst)&(0x0F); 
     } else if (opCode == 0x3) {
+        uint8_t reg = (inst >> 4)&(0x03);
+        uint8_t newPC = inst &(0xF);
+        if (R[0] != R[reg])
+            PC = --newPC; 
+    } else if (opCode == 0x1) {
         printf("PC:%d\n",PC);
         printf("R[0]:%d\n",R[0]);
         printf("R[1]:%d\n",R[1]);
         printf("R[2]:%d\n",R[2]);
         printf("R[3]:%d\n",R[3]);
-        exit(-1);
-    } else 
+    } 
+    else 
         assert("can't recognize opCode");
     PC++;
 }
